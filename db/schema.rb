e7c7a5f170cd70c91883e27584db6801f8e89f8d
陈小朋 <beslow@chenxiaopengdeMacBook-Pro.local>
2014-12-06 17:33:45 +0800

    add field user_id to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206092115) do
+ActiveRecord::Schema.define(version: 20141206093258) do
 
   create_table "companies", force: true do |t|
     t.string   "simple_name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20141206092115) do
     t.date     "send_order_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "prices", force: true do |t|
