4aac5d3db0263ef9125906a1a7c2de075fe50120
陈小朋 <beslow@chenxiaopengdeMacBook-Pro.local>
2015-01-08 23:15:20 +0800

    更改send_date为need_reach_date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150102081123) do
+ActiveRecord::Schema.define(version: 20150108151143) do
 
   create_table "companies", force: true do |t|
     t.string   "simple_name"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150102081123) do
     t.integer  "customer_id"
     t.integer  "supplier_id"
     t.text     "content"
-    t.datetime "send_date"
+    t.datetime "need_reach_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
