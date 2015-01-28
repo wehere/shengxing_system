61adf2b82d657c5a05af9549c5e63b7ba7189927
陈小朋 <beslow@chenxiaopengdeMacBook-Pro.local>
2015-01-28 21:56:19 +0800

    单据下一个，考虑到正单员工餐

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,14 +11,28 @@ class Order < ActiveRecord::Base
     self.order_items.sum('money').round(2)
   end
 
-  def previous
-    orders = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id).where("reach_order_date < ?", self.reach_order_date)
-    orders.where(reach_order_date: orders.maximum('reach_order_date')).first
+  def previous previous_type
+    if previous_type.blank?
+      orders = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id).where("reach_order_date < ?", self.reach_order_date)
+      orders = orders.order(:customer_id).order(:store_id).order(order_type_id: :desc)
+      orders.where(reach_order_date: orders.maximum('reach_order_date')).first
+    else
+      previous_order = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id, order_type_id: previous_type.id, reach_order_date: self.reach_order_date).first
+      return self.previous( previous_type.previous ) if previous_order.blank?
+      previous_order
+    end
   end
 
-  def next
-    orders = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id).where("reach_order_date > ?", self.reach_order_date)
-    orders.where(reach_order_date: orders.minimum('reach_order_date')).first
+  def next next_type
+    if next_type.blank?
+      orders = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id).where("reach_order_date > ?", self.reach_order_date)
+      orders = orders.order(:customer_id).order(:store_id).order(order_type_id: :asc)
+      orders.where(reach_order_date: orders.minimum('reach_order_date')).first
+    else
+      next_order = Order.valid_orders.where(customer_id: self.customer_id, store_id: self.store_id, supplier_id: self.supplier_id, order_type_id: next_type.id, reach_order_date: self.reach_order_date).first
+      return self.next( next_type.next ) if next_order.blank?
+      next_order
+    end
   end
 
 end
--- app/models/order_type.rb
+++ app/models/order_type.rb
@@ -3,4 +3,36 @@ class OrderType < ActiveRecord::Base
   belongs_to :supplier, class_name: 'Company', foreign_key: 'supplier_id'
   belongs_to :customer, class_name: 'Company', foreign_key: 'customer_id'
   validates_presence_of :name
+
+  def previous
+    all_order_types = self.customer.get_order_types.order(:id)
+    count = all_order_types.count
+    return nil if count == 1 or count == 0
+    pos = -1
+    0.upto count-1 do |i|
+      if all_order_types[i].name == self.name
+        pos = i
+        break
+      end
+    end
+    return nil if pos == -1
+    return nil if pos == 0
+    return all_order_types[pos - 1]
+  end
+
+  def next
+    all_order_types = self.customer.get_order_types.order(:id)
+    count = all_order_types.count
+    return nil if count == 1 or count == 0
+    pos = -1
+    0.upto count-1 do |i|
+      if all_order_types[i].name == self.name
+        pos = i
+        break
+      end
+    end
+    return nil if pos == -1
+    return nil if pos == count - 1
+    return all_order_types[pos + 1]
+  end
 end
